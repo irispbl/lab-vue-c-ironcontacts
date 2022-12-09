28f9d2f62d3329e958abe19044abfb9dba2ad3d4
Iris Pascual <irispblg@gmail.com>
2022-12-09 18:51:53 +0100

    add lab5

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,63 @@
+// src/App.vue
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>IronContacts</h1>
+  <button type="button" @click="addNewContact()"> Add Random Contact</button>
+  <button type="button">Sort by popularity</button>
+  <button type="button" @click="sortContacts()">Sort by name</button>
+  <table>
+    <thead>
+      <tr>
+        <th>picture</th>
+        <th>name</th>
+        <th>popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
+        
+      </tr>
 
-  <i class="fas fa-trophy"></i>
+      <tr v-for="contact in contactList" :key="id">
+        <td><img :src="contact.pictureUrl" :alt="contact.pictureUrl" width="70"/></td>
+        <td>{{ contact.name }}</td>
+        <td>{{ contact.popularity }}</td>
+        <td v-if="contact.wonOscar===true"> <i class="fas fa-trophy"></i></td>
+        <td v-if="contact.wonEmmy ===true"> <i class="fas fa-trophy"></i></td>
+      </tr>
+    </thead>
+  </table>
 </template>
 
 <script>
+import contacts from "./contacts.json";
 export default {
-
-}
+  data() {
+    return {
+      contacts: contacts,
+      contactList: contacts.slice(0,5),
+    };
+  },
+  methods: {
+    addNewContact (){
+    const randomContact = Math.floor(Math.random() * contacts.length);
+    if(!this.contactList.includes(contacts[randomContact])){
+      this.contactList.push(contacts[randomContact]);
+    }
+   },
+   sortContacts(){
+    this.contactList.sort(function (a,b){
+      if (b > a){
+        return -1;
+      } if (a > b){
+        return 1;
+      }
+      return 0;
+    })
+   }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
+
+<!--deleteElement(indice){
+  contactList.splice(indice,1);
+ },-->
